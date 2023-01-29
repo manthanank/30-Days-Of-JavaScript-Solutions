57c8e4cab463b1fa8e17c7b4eb8b2fcec3ab629d
Manthan Ankolekar <manthan.ank@gmail.com>
2023-01-29 16:07:11 +0530

    day 1 completed

---
+++ Day1/script.js
@@ -0,0 +1,29 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+var variable1;
+var variable2;
+var variable3;
+var variable4;
+
+var variable1 = "value1";
+var variable2 = "value2";
+var variable3 = "value3";
+var variable4 = "value4";
+
+var first_name = "John";
+var last_name = "Doe";
+var marital_status = "single";
+var country = "United States";
+var age = 30;
+
+var first_name = "John", last_name = "Doe", marital_status = "single", country = "United States", age = 30;
+
+var myAge = 25;
+var yourAge = 30;
+console.log(myAge);
+console.log(yourAge);
